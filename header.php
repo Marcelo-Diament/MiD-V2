06c37438517085a4d5a511bf8cde6011f1122b5b
Marcelo-Diament <marcelodiament@gmail.com>
2018-09-11 16:51:05 -0300

    Commit Inicial | Homologação v2 (refatoração do zero)

---
+++ header.php
@@ -0,0 +1,38 @@
+	<body>
+		<nav class="container-fluid">
+			<section class="row menu-superior">
+				<article class="col-4">
+					<h2><strong>móveis e design autoral para decorar seu ambiente</strong></h2>
+				</article>
+				<article class="col-4 contato-topo">
+					<a href="mailto:contato@deezign.com.br?Subject=Deezign - Contato via site" rel="nofollow" title="Enviar email para Deezign">contato@deezign.com.br</a>
+				</article>
+				<article class="col-4 login-register">
+					<a href="#" rel="next" title="Acesse sua conta ou registre-se"><b>acesse ou crie sua conta aqui </b><i class="fas fa-user-circle"></i></a>
+					<a href="#" rel="next" title="Personalize sua navegação, escolha uma das cores disponíveis"><i class="fas fa-paint-brush"></i></a>
+				</article>
+			</section>
+			<section class="row menu-principal">
+				<article class="col-2">
+					<a href="#" rel="next" title="Acessar tela inicial">
+						<img src="favicons/logo-deezign-140x40.png" alt="Deezign | Móveis e Design Autoral" title="Deezign | Móveis e Design Autoral" height="40" width="140">
+					</a>
+				</article>
+				<article class="col-8">
+					<ul>
+						<li><a href="#" rel="next" title="Móveis feitos por designers renomados" class="active" data-toggle="dropdown">Móveis</a></li>
+						<li><a href="#" rel="next" title="Objetos de decoração de design autoral">Decoração</a></li>
+						<li><a href="#" rel="next" title="Móveis e decoração infantil com design assinado">Deezign.<span class="kids">kids</span></a></li>
+						<li><a href="#" rel="next" title="Coleções, Designers e Marcas">Coleções</a></li>
+						<li><a href="#" rel="next" title="Veja os produtos organizados por vitrines">Vitrines</a></li>
+						<li><a href="#" rel="next" title="Entre em contato conosco">Contato</a></li>
+						<li><a href="#" rel="next" title="Confira algumas das fotos da Deezign no Instagram">Insta</a></li>
+					</ul>
+				</article>
+				<article class="col-2 contatos">
+					<a href="#" rel="next" title="Confira os produtos selecionados"><i class="fal fa-shopping-bag"></i></a>
+					<a href="#" rel="next" title="Busque o produto desejado"><i class="fal fa-search"></i></a>
+					<a href="#" rel="next" title="Entre em contato com a gente"><i class="fas fa-comment"></i></a>
+				</article>
+			</section>
+		</nav>
---
+++ produto.php
@@ -0,0 +1,174 @@
+		<?php include_once('head.php'); ?>
+		<title>poltrona Tripolina S lona</title>
+		<meta name="title" content="poltrona Tripolina S lona">
+		<meta name="description" content="A Poltrona Tripolina, de design assinado pela Futon Company, destaca-se pelo conforto, beleza e portabilidade - é uma poltrona dobrável fácil de montar e transportar. Peça disponível em várias cores e dois tamanhos para decorar seu ambiente.">
+		<meta name="keywords" content="poltrona, tripolina, cadeira, poltrona desmontável, cadeira desmontável, cadeira estilo camping, cadeira design autoral, cadeira design assinado, cadeira lona, cadeira madeira e lona, cadeira madeira, cadeira dobrável">
+	</head>
+	<?php include_once('header.php'); ?>
+	<main class="container-fluid">
+		<section class="row banner-pdt-detalhe">
+			<div>
+				<h1><span class="prefix">poltrona</span> Tripolina S <span class="sufix">lona</span></h1>
+				<h2>Poltrona Dobrável estilo camping com design assinado pelo Estúdio Futon Company</h2>
+			</div>
+		</section>
+		<section class="row breadcrumbList">
+			<ol itemscope itemtype="http://schema.org/BreadcrumbList">
+			  <li itemprop="itemListElement" itemscope
+			      itemtype="http://schema.org/ListItem">
+			    <a itemprop="item" href="https://deezign.com.br" rel="next" title="Acessar Homepage">
+			    <span itemprop="name">Início</span></a>
+			    <meta itemprop="position" content="1" />
+			  </li>
+			  <li itemprop="itemListElement" itemscope
+			      itemtype="http://schema.org/ListItem">
+			    <a itemprop="item" href="https://deezign.com.br/shop" rel="next" title="Ver Todos os Produtos">
+			    <span itemprop="name">Shop</span></a>
+			    <meta itemprop="position" content="2" />
+			  </li>
+			  <li itemprop="itemListElement" itemscope
+			      itemtype="http://schema.org/ListItem">
+			    <a itemprop="item" href="https://deezign.com.br/shop/moveis" rel="next" title="Ver Todos os Móveis">
+			    <span itemprop="name">Móveis</span></a>
+			    <meta itemprop="position" content="3" />
+			  </li>
+			  <li itemprop="itemListElement" itemscope
+			      itemtype="http://schema.org/ListItem">
+			    <a itemprop="item" href="https://deezign.com.br/shop/moveis/cadeiras-e-poltronas" rel="next" title="Ver Todas as Cadeiras e Poltronas">
+			    <span itemprop="name">Cadeiras e Poltronas</span></a>
+			    <meta itemprop="position" content="4" />
+			  </li>
+			  <li itemprop="itemListElement" itemscope
+			      itemtype="http://schema.org/ListItem">
+			    <a itemprop="item" href="#" rel="next" title="Você está visualizando a poltrona Tripolina S lona" class="active">
+			    <span itemprop="name" class="active">Poltrona Tripolina S</span></a>
+			    <meta itemprop="position" content="5" />
+			  </li>
+			</ol>
+		</section>
+		<span class="clearfix"></span>
+		<section class="row">
+			<aside class="col-12 col-md-2" id="categorias-menu">
+				<ul class="card">
+					<li class="card-header">
+						<a href="#categorias-moveis" class="card-link active" data-toggle="collapse" rel="next" title="Ver Todos os Móveis"><strong>Móveis</strong><i class="fas fa-sort-down"></i></a>
+					</li>
+					<ul id="categorias-moveis" class="card-body collapse show" data-parent="#categorias-menu">
+						<li><a href="#" rel="next" title="Ver Futons"><strong>Futons</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Sofá-camas"><strong>Sofá-camas</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Camas"><strong>Camas</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Mesas"><strong>Mesas</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Cadeiras e Poltronas" class="active"><strong>Cadeiras e Poltronas</strong></a></li>
+					</ul>
+					<li class="card-header">
+						<a href="#categorias-acessorios" class="card-link" data-toggle="collapse" rel="next" title="Ver Todos os Acessórios"><strong>Acessórios</strong><i class="fas fa-sort-down"></i></a>
+					</li>
+					<ul id="categorias-acessorios" class="card-body collapse" data-parent="#categorias-menu">
+						<li><a href="#" rel="next" title="Ver Almofadas"><strong>Almofadas</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Luminárias"><strong>Luminárias</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Acessórios"><strong>Acessórios</strong></a></li>
+					</ul>
+					<li class="card-header">
+						<a href="#categorias-infantil" class="card-link" data-toggle="collapse" rel="next" title="Ver Todos os Produtos Infantis"><strong>Deezign.<span class="kids">kids</span></strong><i class="fas fa-sort-down"></i></a>
+					</li>
+					<ul id="categorias-infantil" class="card-body collapse" data-parent="#categorias-menu">
+						<li><a href="#" rel="next" title="Ver Almofadas"><strong>Almofadas</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Berços"><strong>Berços</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Mesas e Assentos"><strong>Mesas e Assentos</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Sofá-Camas"><strong>Sofá-Camas</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Pelúcias e Bonecas"><strong>Pelúcias e Bonecas</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Decoração"><strong>Decoração</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Casas de Bonecas"><strong>Casas de Bonecas</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Brinquedos e Jogos"><strong>Brinquedos e Jogos</strong></a></li>
+					</ul>
+					<li class="card-header">
+						<a href="#categorias-colecoes" class="card-link" data-toggle="collapse" rel="next" title="Ver Todas as Coleções"><strong>Coleções</strong><i class="fas fa-sort-down"></i></a>
+					</li>
+					<ul id="categorias-colecoes" class="card-body collapse" data-parent="#categorias-menu">
+						<li><a href="#" rel="next" title="Ver Produtos da Coleção Casual"><strong>Casual</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos da Coleção Contemporâneo"><strong>Contemporâneo</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos da Coleção Design Assinado"><strong>Design Assinado</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos da Coleção Vintage"><strong>Vintage</strong></a></li>
+					</ul>
+					<li class="card-header">
+						<a href="#categorias-designers" class="card-link" data-toggle="collapse" rel="next" title="Ver Todos os Designers"><strong>Designers</strong><i class="fas fa-sort-down"></i></a>
+					</li>
+					<ul id="categorias-designers" class="card-body collapse" data-parent="#categorias-menu">
+						<li><a href="#" rel="next" title="Ver Produtos Assinados por André Vainer"><strong>André Vainer</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos Assinados por Coletivo Traquitana"><strong>Coletivo Traquitana</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos Assinados por Estúdio Futon Company"><strong>Estúdio Futon Company</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos Assinados por Max Sauze"><strong>Max Sauze</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos Assinados por Paulo Mendes da Rocha"><strong>Paulo Mendes da Rocha</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos Assinados por Mauricio Klabin"><strong>Mauricio Klabin</strong></a></li>
+					</ul>
+					<li class="card-header">
+						<a href="#categorias-marcas" class="card-link" data-toggle="collapse" rel="next" title="Ver Todas as Marcas"><strong>Marcas</strong><i class="fas fa-sort-down"></i></a>
+					</li>
+					<ul id="categorias-marcas" class="card-body collapse" data-parent="#categorias-menu">
+						<li><a href="#" rel="next" title="Ver Produtos da Marca Acierno"><strong>Acierno</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos da Marca Bododo"><strong>Bododo</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos da Marca Ekilux"><strong>Ekilux</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos da Marca Futon Company"><strong>Futon Company</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos da Marca Objekto"><strong>Objekto</strong></a></li>
+					</ul>
+					<li class="card-header">
+						<a href="#categorias-vitrines" class="card-link" data-toggle="collapse" rel="next" title="Ver Todas as Vitrines"><strong>Vitrines</strong><i class="fas fa-sort-down"></i></a>
+					</li>
+					<ul id="categorias-vitrines" class="card-body collapse" data-parent="#categorias-menu">
+						<li><a href="#" rel="next" title="Ver Produtos da Marca Acierno"><strong>Acierno</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos da Marca Bododo"><strong>Bododo</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos da Marca Futon Company"><strong>Futon Company</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos da Marca Mon Petit Art"><strong>Mon Petit Art</strong></a></li>
+						<li><a href="#" rel="next" title="Ver Produtos da Marca Sam & Peas"><strong>Sam & Peas</strong></a></li>
+					</ul>
+				</ul>
+			</aside>
+			<article class="col-12 col-md-5 produto-imagens">
+				<a href="#" title="Clique para ampliar a imagem da Tripolina S" rel="next"><i class="fal fa-search"></i></a>
+				<img class="imagem-destaque" src="img/sample/poltrona-tripolina-s-lona-por-estudio-futon-company-01b.jpg" title="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" alt="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" width="100%" height="auto">
+				<ul class="produtos-thumbs col-12">
+					<li><img src="img/sample/poltrona-tripolina-s-lona-por-estudio-futon-company-01.jpg" title="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" alt="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" width="100px" height="auto"></li>
+					<li><img src="img/sample/poltrona-tripolina-s-lona-por-estudio-futon-company-01b.jpg" title="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" alt="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" width="100px" height="auto"></li>
+					<li><img src="img/sample/poltrona-tripolina-s-lona-por-estudio-futon-company-02.jpg" title="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" alt="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" width="100px" height="auto"></li>
+					<li><img src="img/sample/poltrona-tripolina-s-lona-por-estudio-futon-company-03.jpg" title="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" alt="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" width="100px" height="auto"></li>
+					<li><img src="img/sample/poltrona-tripolina-s-lona-por-estudio-futon-company-04.jpg" title="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" alt="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" width="100px" height="auto"></li>
+					<li><img src="img/sample/poltrona-tripolina-s-lona-por-estudio-futon-company-05.jpg" title="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" alt="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" width="100px" height="auto"></li>
+					<li><img src="img/sample/poltrona-tripolina-s-lona-por-estudio-futon-company-06.jpg" title="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" alt="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" width="100px" height="auto"></li>
+					<li><img src="img/sample/poltrona-tripolina-s-lona-por-estudio-futon-company-07.jpg" title="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" alt="Poltrona Tripolina Lona | desgin assinado por Estúdio Futon Company" width="100px" height="auto"></li>
+				</ul>
+			</article>
+			<article class="col-12 col-md-4 produto-descricao">
+				<div>
+					<h3>sobre a <strong><span class="prefix">poltrona</span><br/>Tripolina S</strong></h3>
+					<div class="texto-descritivo">
+						<p>Clássica Camp Chair com assento em tecido de algodão: Tripolina Tauari Lonita. Releitura atual de um ícone do design internacional, que foi criado em 1877 e usado depois pelos oficiais ingleses durante a campanha de Tripoli.</p>
+						<p>Nossa Tripolina é equipada de dobradiça especialmente desenvolvidas para facilitar o mecanismo de abre/fecha do móvel e seu armazenamento numa sacola cru com alça.</p>
+						<p>Portátil, com seu look vintage, a Tripolina tem estrutura em Tauari maciça e assento em lonita cru ou colorida. Muito prática, Tripolina fica perfeita na sala, no quarto ou na varanda. Dobrável e armazenável, é um assento extra que pode até levar na casa da praia ou no picnic na praça.</p>
+						<p>A capa de lonita pode ser adquirida avulsa.</p>
+					</div>
+				</div>
+				<div class="medidas row">
+					<h4 class="col-12">medidas da poltrona</h4>
+					<div class="col-3 altura">
+						<img src="img/common/altura.png" alt="Altura da poltrona Tripolina S" title="Altura da poltrona Tripolina S" id="altura-pdt-img" height="50px" width="auto">
+						<p>Altura:<br/><span id="altura-pdt-cm">80 cm</span></p>
+					</div>
+					<div class="col-3 largura">
+						<img src="img/common/largura.png" alt="Largura da poltrona Tripolina S" title="Largura da poltrona Tripolina S" id="largura-pdt-img" height="50px" width="auto">
+						<p>Largura:<br/><span id="largura-pdt-cm">40 cm</span></p>
+					</div>
+					<div class="col-3 profundidade">
+						<img src="img/common/profundidade.png" alt="Profundidade da poltrona Tripolina S" title="Profundidade da poltrona Tripolina S" id="profundidade-pdt-img" height="50px" width="auto">
+						<p>Profundidade:<br/><span id="profundidade-pdt-cm">60 cm</span></p>
+					</div>
+					<div class="col-3 peso">
+						<img src="img/common/peso.png" alt="Peso da poltrona Tripolina S" title="Peso da poltrona Tripolina S" id="peso-pdt-img" height="50px" width="auto">
+						<p>Peso:<br/><span id="peso-pdt-g">2.060 g</span></p>
+					</div>
+				</div>
+			</article>
+		</section>
+
+
+	</main>
+<?php include_once('footer.php'); ?>
